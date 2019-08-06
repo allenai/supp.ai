619b04dcd3b964781b392015630d817cead3f870
Sam Skjonsberg <sams@allenai.org>
2019-08-06 10:39:31 -0700

    Add a request log.

---
+++ ui/server.js
@@ -0,0 +1,35 @@
+/**
+ * NOTE: This file is not transpiled, and is only executed on the server.
+ * One should accordingly stick to writing code compatible with the target
+ * NodeJS version (see the ui/Dockerfile).
+ */
+
+const { parse } = require('url');
+const next = require('next');
+const morgan = require('morgan');
+const express = require('express');
+
+const server = express();
+
+const isNotProd = process.env.NODE_ENV !== 'production';
+const app = next({ dev: isNotProd });
+
+// We define a custom format that's used in production that makes our request
+// logs parseable (and thereby searchable) using Google's logging platform.
+morgan.format('json', JSON.stringify({
+    status: ':status',
+    method: ':method',
+    url: ':url',
+    response_time: ':response-time ms'
+}));
+
+app.prepare().then(() => {
+    const format = process.env.SUPPAIR_LOG_FORMAT || (isNotProd ? 'tiny' : 'json');
+    const port = process.env.SUPPAI_UI_PORT || 3000;
+    server
+        .use(morgan(format))
+        .use(app.getRequestHandler())
+        .listen(port, () => {
+            console.log(`listening on http://localhost:${port}`)
+        });
+});
