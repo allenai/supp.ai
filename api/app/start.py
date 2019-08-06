ec0ffa5e67a5a72fd43dfa7f239cfd857ed62234
Sam Skjonsberg <sams@allenai.org>
2019-08-06 13:55:18 -0700

    Add continuous integration support. (#3)
    Add continuous integration support.
    
    This PR adds a cloudbuild file that defines steps to be executed
    whenever a pull request is opened.
    
    Whenever a PR is opened, we:
    
    * Build an API docker image
    * Build a UI docker image
    * Use `mypy` to statically analyze the API codebase
    * Use `black` to check for formatting errors in the API codebase
    * Use `prettier` to check for formatting errors in the UI codebase

--- api/app/start.py
+++ api/app/start.py
@@ -3,10 +3,11 @@ import os
 import sys
 import logging
 from typing import Tuple
-from gevent.pywsgi import WSGIServer
+from gevent.pywsgi import WSGIServer  # type: ignore
 from flask import Flask, Response, request, jsonify
 from app.api import create_api
 from app.utils import StackdriverJsonFormatter
+
 
 def start():
     """
@@ -14,19 +15,15 @@ def start():
     in development mode (which is ideal for local development but unideal
     for production use).
     """
-    parser = argparse.ArgumentParser(description='Starts your application\'s HTTP server.')
-    parser.add_argument(
-        '--port',
-        '-p',
-        help='The port to listen on',
-        default=8000
+    parser = argparse.ArgumentParser(
+        description="Starts your application's HTTP server."
     )
+    parser.add_argument("--port", "-p", help="The port to listen on", default=8000)
     parser.add_argument(
-        '--prod',
-        help=
-            'If specified the server is started in production mode, where ' +
-            'the server isn\'t restarted as changes to the source code occur.',
-        action='store_true'
+        "--prod",
+        help="If specified the server is started in production mode, where "
+        + "the server isn't restarted as changes to the source code occur.",
+        action="store_true",
     )
     args = parser.parse_args()
 
@@ -39,11 +36,10 @@ def start():
     if args.prod:
         json_handler = logging.StreamHandler()
         json_handler.setFormatter(StackdriverJsonFormatter())
-        handlers = [ json_handler ]
+        handlers = [json_handler]
 
     logging.basicConfig(
-        level=os.environ.get('LOG_LEVEL', default=logging.INFO),
-        handlers=handlers
+        level=os.environ.get("LOG_LEVEL", default=logging.INFO), handlers=handlers
     )
     logger = logging.getLogger()
     logger.debug("AHOY! Let's get this boat out to water...")
@@ -53,19 +49,21 @@ def start():
     # Bind the API functionality to our application. You can add additional
     # API endpoints by editing api.py.
     logger.debug("Starting: init API...")
-    app.register_blueprint(create_api(), url_prefix='/')
+    app.register_blueprint(create_api(), url_prefix="/")
     logger.debug("Complete: init API...")
 
     # In production we use a HTTP server appropriate for production.
     if args.prod:
         logger.debug("Starting: gevent.WSGIServer...")
-        http_server = WSGIServer(('0.0.0.0', args.port), app, log=logger,
-            error_log=logger)
-        app.logger.info(f'Server listening at http://0.0.0.0:{args.port}')
+        http_server = WSGIServer(
+            ("0.0.0.0", args.port), app, log=logger, error_log=logger
+        )
+        app.logger.info(f"Server listening at http://0.0.0.0:{args.port}")
         http_server.serve_forever()
     else:
         logger.debug("Starting: Flask development server...")
-        app.run(host='0.0.0.0', port=args.port)
+        app.run(host="0.0.0.0", port=args.port)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     start()
