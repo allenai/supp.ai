5268f18661eda4f2a801dc002c9f2bf4d66c337b
Sam Skjonsberg <sams@allenai.org>
2019-08-19 16:09:11 -0700

    Add a `robots.txt` forbidding crawlers.
    Re #50; Forbid crawlers for now.

--- ui/server.js
+++ ui/server.js
@@ -8,11 +8,14 @@ const next = require('next');
 const morgan = require('morgan');
 const express = require('express');
 const url = require('url');
+const path = require('path');
 
 const server = express();
 
 const isNotProd = process.env.NODE_ENV !== 'production';
 const app = next({ dev: isNotProd });
+
+server.disable('x-powered-by');
 
 // We define a custom format that's used in production that makes our request
 // logs parseable (and thereby searchable) using Google's logging platform.
@@ -27,6 +30,14 @@ app.prepare().then(() => {
     const format = process.env.SUPPAIR_LOG_FORMAT || (isNotProd ? 'tiny' : 'json');
     const port = process.env.SUPPAI_UI_PORT || 3000;
     server.use(morgan(format));
+
+    // Serve the robots.txt when it's requested from the root.
+    server.get('/robots.txt', (req, res) => {
+        return res.status(200).sendFile(
+            path.join(__dirname, 'static', 'robots.txt'),
+            { headers: { 'Content-Type': 'text/plain; charset=utf-8' } }
+        );
+    });
 
     // NextJS requires that dynamic urls be configured with a bit of extra
     // magic. This sets up the routing required for detail pages.
