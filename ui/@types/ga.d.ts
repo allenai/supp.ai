838d4ada1cce215a21f952f9c0579f8ad7ea66f2
Sam Skjonsberg <sams@allenai.org>
2019-10-10 20:44:43 -0700

    Make sure pageviews are tracked when the page changes.
    Unfortunately Google Analytics wasn't tracking pageviews when
    a page change occured asynchronously. This resolves that by wiring
    up to an event that's triggered with every page change.

---
+++ ui/@types/ga.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Extend the browser's Window type to reflect the additional APIs associated
+ * with Google Analytics.
+ */
+export declare global  {
+    interface Window {
+        gtag?: (...args: any[]) => void;
+        googleAnalyticsId?: string;
+    }
+}
--- ui/pages/_app.tsx
+++ ui/pages/_app.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import Router from "next/router";
 import App, { Container } from "next/app";
 import Head from "next/head";
 import { ThemeProvider } from "@allenai/varnish/theme";
@@ -7,13 +8,38 @@ import { ThemeProvider } from "@allenai/varnish/theme";
  * A custom NextJS App with a fresh coat o' Varnish 🎨⛵️.
  */
 export default class AppWithVarnishTheme extends App {
+    trackPageView(path: string) {
+        if (process.env.NODE_ENV === "production") {
+            // We use `setTimeout` so that the `document.title` is *likely*
+            // already updated. This appears to work without issue, but could
+            // in theory expose a race condition where the recorded title
+            // is that of the previous page.
+            setTimeout(() => {
+                if (window.gtag && window.googleAnalyticsId) {
+                    window.gtag('config', window.googleAnalyticsId, {
+                        page_title : document.title,
+                        page_path: path
+                    });
+                }
+            }, 0);
+        } else {
+            console.log('Page change not tracked - analytics disabled.');
+        }
+    }
+    componentDidMount() {
+        Router.events.on('routeChangeComplete', this.trackPageView);
+    }
+    componentWillUnmount() {
+        Router.events.off('routeChangeComplete', this.trackPageView);
+    }
     render() {
         const { Component, pageProps } = this.props;
         const gaScript = [
+            "window.googleAnalyticsId = 'UA-136317607-8';",
             "window.dataLayer = window.dataLayer || [];",
             "function gtag(){ dataLayer.push(arguments); }",
             "gtag('js', new Date());",
-            "gtag('config', 'UA-136317607-8');"
+            "gtag('config', window.googleAnalyticsId);"
         ].join("\n");
         return (
             <React.Fragment>
