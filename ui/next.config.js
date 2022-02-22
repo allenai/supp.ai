af0872fb6d5d3ee202a88c9c3ee221e60799282f
Sam Skjonsberg <sams@allenai.org>
2022-02-21 17:13:39 -0800

    Update NextJS (and a bunch of other things)
    This applies a bunch of dependency updates, mainly to close
    up a few CVEs.
    
    The biggest motivator for these changes is a CVE with NextJS's
    image transformation API, which is why we both update Next and
    add rules to the proxy that prevent that endpoint from being
    accessible.
    
    I was able to get the site up and working with these changes.
    I clicked around and everything looked good, so I think this
    is safe.
    
    https://github.com/allenai/reviz/issues/402

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,7 +1,11 @@
 const withImages = require('next-images');
-const withTranspiledModules = require('next-transpile-modules');
+const withTranspiledModules = require('next-transpile-modules')([ '@allenai/varnish' ]);
 
 module.exports = withImages(withTranspiledModules({
-    transpileModules: [ '@allenai/varnish' ],
+    /* Disabling NextJS v11 default static image import system in favor of plugin
+       https://nextjs.org/docs/upgrading#nextconfigjs-customization-to-import-images */
+    images: {
+        disableStaticImages: true,
+    },
     poweredByHeader: false
 }));
